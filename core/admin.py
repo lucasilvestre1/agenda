cbd2456b29428edcd9ad6711384ae33c7b1e80cb
lucasilvestre1 <lucas.silvestre07@gmail.com>
2021-04-14 01:46:02 -0300

    Commit com inclusão de tabela de eventos já com tabela auth.user como FK

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'user', 'data_evento', 'create_date']
+    list_filter = ['titulo', 'user', 'data_evento']
+
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Evento(models.Model):
+
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    create_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # python manage.py createsuperuser
+    # python manage.py makemigrations core
+    # python manage.py sqlmigrate core 0001
+    # python manage.py migrate core 0001
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
