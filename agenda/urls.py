43c92c93a4ca5cb73f8c176b23f632d2c49b8c95
lucasilvestre1 <lucas.silvestre07@gmail.com>
2021-04-15 00:53:02 -0300

    inclusão das páginas HTML no projeto

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('evento/<titulo>', views.local_evento),
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 # Create your views here.
+
+
+def index(requests):
+    return redirect('/agenda/')
 
 
 def local_evento(requests, titulo=None):
     evento = Evento.objects.get(titulo=titulo)
     return HttpResponse('%s foi definido como local para o Evento %s.' % (evento.local, titulo))
 
+
+def lista_eventos(request):
+    user = request.user
+    eventos = Evento.objects.filter(user=user)
+    response = {
+        'user': eventos[0].user,
+        'eventos': eventos,
+    }
+    return render(request, 'agenda.html', response)
